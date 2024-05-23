0fa1431e84d445c43c0e4e15021ad66f7775e286
João Manoel <64815276+Joaomanooel@users.noreply.github.com>
2024-05-23 14:07:00 -0300

    Exercicio de JavaScript 2
    Primeira parte.

---
+++ exercicio-javascript/exercicio-02/index.js
@@ -0,0 +1,10 @@
+const nome1 = prompt("Nome do primeiro veiculo")
+const velocidade1 = prompt("Velocidade do primeiro veiculo")
+const nome2 = prompt("Nome do primeiro veiculo")
+const velocidade2 = prompt("Velocidade do primeiro veiculo")
+
+if (velocidade1 === velocidade2){
+    alert("Velocidade iguais")
+} else if (velocidade1 !== velocidade2){
+    alert("Velocidade diferente")
+}
