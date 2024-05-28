382908f52f1249264f6ae0773a86ec3f04391fc6
João Manoel <64815276+Joaomanooel@users.noreply.github.com>
2024-05-28 12:56:43 -0300

    Parte 2 do exercício
    finalizando o exercício de Java Script

--- exercicio-javascript/exercicio-02/index.js
+++ exercicio-javascript/exercicio-02/index.js
@@ -1,10 +1,12 @@
 const nome1 = prompt("Nome do primeiro veiculo")
-const velocidade1 = prompt("Velocidade do primeiro veiculo")
-const nome2 = prompt("Nome do primeiro veiculo")
-const velocidade2 = prompt("Velocidade do primeiro veiculo")
+const velocidade1 = parseFloat(prompt("Velocidade do primeiro veiculo"))
+const nome2 = prompt("Nome do segundo veiculo")
+const velocidade2 = parseFloat(prompt("Velocidade do segundo veiculo"))
 
-if (velocidade1 === velocidade2){
-    alert("Velocidade iguais")
-} else if (velocidade1 !== velocidade2){
-    alert("Velocidade diferente")
+if (velocidade1 > velocidade2){
+    alert(nome1 + " é mais rapido que o " + nome2)
+}else if (velocidade2 > velocidade1){
+    alert(nome2 + " é mais rapido que o " + nome1 )
+}else{
+    alert(nome1 + " e " + nome2 + " possuem velocidade iguais. ")
 }
