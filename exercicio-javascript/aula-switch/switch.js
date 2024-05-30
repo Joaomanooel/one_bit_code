e4f4a3b942fb0cc9e4345aef6a90598f01338b06
João Manoel <64815276+Joaomanooel@users.noreply.github.com>
2024-05-29 22:30:02 -0300

    Aula de switch no JavaScript
    Aula de switch no Java Script

---
+++ exercicio-javascript/aula-switch/switch.js
@@ -0,0 +1,15 @@
+const resultado = prompt("Escolha uma alternativa a seguir: \nA)\nB)\nC)")
+
+switch(resultado){
+    case "A":
+        alert("O resultado é A")
+    break
+    case "B":
+        alert("O resultado é B")
+    break
+    case "C":
+        alert("O resultado é C")
+    break
+    default:
+        alert("Finalizado...")
+}
