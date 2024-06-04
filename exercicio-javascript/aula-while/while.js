2185d983982b3c94f9705d6aad1631509986bd97
João Manoel <64815276+Joaomanooel@users.noreply.github.com>
2024-06-04 12:39:03 -0300

    Atualizando exercício
    Atualizando exercício

--- exercicio-javascript/aula-while/while.js
+++ exercicio-javascript/aula-while/while.js
@@ -4,10 +4,10 @@ while(velocidade > 0){
     alert("O carro está a " + velocidade + "km/h")
     velocidade -= 20
     alert("Diminuindo 20 km/h")
-
-    if(velocidade === 40){
-        break
-    }
+        //Podemos incluir tanto if quanto ifelse-switch e outros
+    // if(velocidade === 40){
+    //    break
+    //}
 }
 
 alert("O carro parou")
